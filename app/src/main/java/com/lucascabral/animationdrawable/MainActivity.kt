60ce0000064c18357f83c054b8b47c56dff771ab
lucascabraldeve <lucas_cabral.deve@outlook.com>
2021-02-15 19:20:41 -0300

    Finalizado

--- app/src/main/java/com/lucascabral/animationdrawable/MainActivity.kt
+++ app/src/main/java/com/lucascabral/animationdrawable/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.lucascabral.animationdrawable
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var animD = AnimationDrawable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        daImageView.setBackgroundResource(R.drawable.animation)
+        animD = daImageView.background as AnimationDrawable
+        animD.start()
     }
 }
